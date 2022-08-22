9301f050252782a01f3441e1424b77e2c1543300
lffwl <wl@ulang.com>
2022-08-22 16:20:00 +0800

    content-type == json

--- middleware/http.go
+++ middleware/http.go
@@ -18,34 +18,40 @@ const (
 func HttpResponse(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		bodyWriter := response.NewResponseBodyWriter(w)
+		if w.Header().Get("content-type") == "text/json" {
 
-		w = bodyWriter
+			bodyWriter := response.NewResponseBodyWriter(w)
 
-		next.ServeHTTP(w, r)
+			w = bodyWriter
 
-		var res response.JsonResponse
-		if err := json.Unmarshal(bodyWriter.GetBodyBytesAndReset(), &res); err != nil {
-			response.Json(w, HttpInternalServiceErrorCode, err.Error())
+			next.ServeHTTP(w, r)
+
+			var res response.JsonResponse
+			if err := json.Unmarshal(bodyWriter.GetBodyBytesAndReset(), &res); err != nil {
+				response.Json(w, HttpInternalServiceErrorCode, err.Error())
+				bodyWriter.OutPut()
+				return
+			}
+
+			switch res.Code {
+			case uerror.HighErrorSuccessCode:
+				res.Code = HttpOKCode
+			case uerror.HighErrorSqlErrorCode:
+			case uerror.HighErrorParamErrorCode:
+			case uerror.HighErrorFileErrorCode:
+			case uerror.HighErrorServiceErrorCode:
+				res.Code = HttpInternalServiceErrorCode
+			case uerror.HighErrorNotAuthCode:
+			case uerror.HighErrorAuthFailedCode:
+				res.Code = HttpUnauthorizedCode
+			}
+
+			response.Json(w, res.Code, res.Message, res.Data)
 			bodyWriter.OutPut()
 			return
 		}
 
-		switch res.Code {
-		case uerror.HighErrorSuccessCode:
-			res.Code = HttpOKCode
-		case uerror.HighErrorSqlErrorCode:
-		case uerror.HighErrorParamErrorCode:
-		case uerror.HighErrorFileErrorCode:
-		case uerror.HighErrorServiceErrorCode:
-			res.Code = HttpInternalServiceErrorCode
-		case uerror.HighErrorNotAuthCode:
-		case uerror.HighErrorAuthFailedCode:
-			res.Code = HttpUnauthorizedCode
-		}
-
-		response.Json(w, res.Code, res.Message, res.Data)
-		bodyWriter.OutPut()
-		return
+		next.ServeHTTP(w, r)
 	})
+
 }
