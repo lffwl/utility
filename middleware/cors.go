21c5923b9362c465fae994608c94e7fc0c7fdbbe
lffwl <wl@ulang.com>
2022-08-23 09:59:46 +0800

    Access-Token

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,7 +7,7 @@ import (
 func Cors(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "content-type,Access-Token")
+		w.Header().Set("Access-Control-Allow-Headers", "content-type, Access-Token")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
 		w.Header().Set("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type, Access-Token")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
